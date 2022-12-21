70cfad78b04d598fc0bd2cff0575274a3fcb791a
annalaura008 <118935086+annalaura008@users.noreply.github.com>
2022-12-21 20:04:28 -0300

    Java Script

---
+++ script.js
@@ -0,0 +1,30 @@
+/*const chocolate = "8"
+if (chocolate >0) {
+    console.log ("positivo")
+} else {
+    console.log ("negativo")
+}*/
+/*let morango = 0
+if (morango === 0) {
+    console.log ("neutro")
+} else if (morango >0) {
+    console.log ("positivo")
+} else {
+    console.log ("negativo")
+}*/
+/*const a = 2
+const b = 4
+console.log(a+b) 
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)*/
+
+/*const a = 20
+const b = 18
+if (a>b) {
+    console.log ("maior")
+}
+else {
+    console.log ("menor")
+}*/
