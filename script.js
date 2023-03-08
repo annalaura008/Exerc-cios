46448ffca19707635f60afbed11ba5f6159792e1
annalaura008 <emmanuellyalves2@gmail.com>
2023-03-08 17:27:42 -0300

    Finalexercicios1

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ else {
     valorDeVenda - custoDeProduto + imposto === lucro
     console.log(lucro * 1000)
 }*/
-const salárioBruto= 3000
+/*const salárioBruto= 3000
 let salárioLíquido= salárioBruto 
 if (salárioBruto >= 0.01 && salárioBruto <= 1556.94) {
     const salário8= 8/100 * salárioBruto 
@@ -157,22 +157,22 @@ else if (salárioBruto < 5189.82) {
    salárioLíquido= saláriomáximo
 }
 
-if (salárioLíquido <=0.01 && salárioLíquido >= 1903.98) {
+if (salárioLíquido >=0.01 && salárioLíquido <= 1903.98) {
 }
-else if (salárioLíquido <= 1903.99 && salárioLíquido >= 2826.65) {
+else if (salárioLíquido >= 1903.99 && salárioLíquido <= 2826.65) {
     const salário7= 7.5/100 * salárioLíquido - 142.80
     salárioLíquido= salárioLíquido - salário7
 }
-else if (salárioLíquido <= 2826.66 && salárioLíquido >= 3751.05) {
+else if (salárioLíquido >= 2826.66 && salárioLíquido <= 3751.05) {
     const salário15= 15/100 * salárioLíquido - 354.80
     salárioLíquido= salárioLíquido - salário15
 }
-else if (salárioLíquido <= 3751.06 && salárioLíquido >= 4664.68) {
+else if (salárioLíquido >= 3751.06 && salárioLíquido <= 4664.68) {
     const salário22= 22.5/100 * salárioLíquido - 636.13
     salárioLíquido= salárioLíquido - salário22
 }
-else if (salárioLíquido < 4664.68) {
+else if (salárioLíquido > 4664.68) {
     const salário27= 27.5/100 * salárioLíquido - 869.36
     salárioLíquido= salárioLíquido - salário27
 }
-    console.log ("tendo R$"+salárioBruto +" como salário bruto, seu salário líquido será de "+salárioLíquido) 
+    console.log ("tendo R$"+salárioBruto +" como salário bruto, seu salário líquido será de "+salárioLíquido)/*
